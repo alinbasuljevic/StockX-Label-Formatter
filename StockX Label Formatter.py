34fe97c87bbe95db807da59bd3f58634bc57967f
Alin Basuljevic <41343001+alinbasuljevic@users.noreply.github.com>
2019-12-02 19:32:25 -0500

    Update StockX Label Formatter.py

--- StockX Label Formatter.py
+++ StockX Label Formatter.py
@@ -1,9 +1,27 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import time
+from colorama import init, Fore, Style
+init(convert=True)
 
-og_file = open('Gucci-Blade-Ship.pdf', 'rb')
+file = input('Enter File Name: ')
+
+og_file = open('{}.pdf'.format(file), 'rb')
 input_file = PdfFileReader(og_file)
 cropped_pdf = PdfFileWriter()
 
 num_of_pages = input_file.getNumPages()
-print (num_of_pages)
 
+page = input_file.getPage(1)
+
+page.cropBox.lowerLeft=(50, 175)
+page.cropBox.upperRight=(600, 455)
+
+
+output = PdfFileWriter()
+output.addPage(page)
+
+with open('{}-Shipping-Label.pdf'.format(file), 'wb') as new:
+    output.write(new)
+
+print (Fore.CYAN + 'Shipping Label Successfully Generated!')
+time.sleep(5)
